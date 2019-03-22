6aabaa588a9676687ff40786e921291257f6e4c1
Petros <psdeme@hotmail.com>
2019-03-22 13:18:39 +0200

    change slice size to 10

--- index.js
+++ index.js
@@ -338,7 +338,7 @@ app.get('/load_dataset/:dt', function (req, res) {
         if (!running) {
             running = true;
             let startTime = microtime.nowDouble();
-            addManyFactsNew(dt,100).then(retval => {
+            addManyFactsNew(dt,10).then(retval => {
                 let endTime = microtime.nowDouble();
                 let timeDiff = endTime - startTime;
                 console.log(retval);
